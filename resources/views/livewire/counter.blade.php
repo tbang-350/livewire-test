94f573bdb5676d659b160e966665ba0de3a23391
tbang-350 <tbway350@gmail.com>
2024-05-07 14:06:58 +0300

    counter works

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,9 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+    <h1>{{ $count }}</h1>
+
+    <button wire:click='increment'>+</button>
+
+    <button wire:click='decrement'>-</button>
+
 </div>
